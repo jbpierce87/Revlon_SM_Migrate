35770dd5695d9b30e5de459bc3a7c65ce0516cb1
jbpierce87 <bradley.pierce2@gmail.com>
2013-09-11 22:03:00 -0400

    Completed assign snapsched to na07

--- VSM_Migrate.ps1
+++ VSM_Migrate.ps1
@@ -32,53 +32,8 @@
 #    [bool]$cleanup = $true
 # )
 
-import-module DataONTAP
-
-### Controller Login Variables
-
-$ntap30 = "usedn-na30"
-$ntap50 = "usedn-na50"
-$ntap07 = "usoxf-na07"
-
-$ntapuser = "root"
-$ntappw = "AcmeL4b#"
-
-### Convert Password to plain text
-$pw = convertto-securestring $ntappw -asplaintext -force
-$cred = new-object -typename system.management.automation.pscredential -argumentlist $ntapuser,$pw
-
-### Connect to the controllers 
-$na07 = Connect-NaController $ntap07 -Credential $cred -https
-$na30 = Connect-NaController $ntap30 -Credential $cred -https
-$na50 = Connect-NaController $ntap50 -Credential $cred -https
-
-### Get volumes from the controllers as a test
-get-navol -controller $na07
-get-navol -controller $na30
-get-navol -controller $na50
-
-### Set aggregate variable where new volumes are going to be created
-$na07_aggr = "aggr9g"
-
-
-### Get volumes from na07 except for clones and vol0
-$na07_vols = Get-NaVol -Controller $na07 | ? {$_.CloneParent -eq $null} | ? {$_.Name -ne "vol0"} 
-
-### Get volume properties for each volume
-foreach ($vol in $na07_vols) {
-
-    $size = Get-NaVolSize -Controller $na07 -Name $vol.Name
-    $guarantee = ((Get-NaVolOption -controller $na07 -Name $vol.Name) | ? { $_.Name -eq "actual_guarantee" }).Value
-    $fracres_percent = ((Get-NaVolOption -Controller $na07 -Name $vol.Name) | ? { $_.Name -eq "fractional_reserve" }).Value
-    $snapres_percent = (Get-NaSnapshotReserve -Controller $na07 -TargetName $vol.Name).Percentage
-
-
-### Create the new volume and set guarantees appropriately on na50
-    New-NaVol -Controller $na50 -Name $vol.Name -Aggregate $na07_aggr -size $size.VolumeSize -SpaceReserve $guarantee }
-
 ### Get Snapmirror schedules from na30 and assign variables
-$snapschedna30 = Get-NaSnapmirrorSchedule -Controller $na30
-
+$snapschedna30 = Get-NaSnapmirrorSchedule -Controller $na30 | select-object Source | out-string
 $snapdestna50 = get-navol -Controller $na50 | ? {$_.CloneParent -eq $null} | ? {$_.Name -ne "vol0"} | foreach-object { 
     $_.Name 
     $destna50 = "usoxf-na07:/vol/" + $_.Name } 
@@ -95,20 +50,5 @@ foreach ($snapsource in $snapschedna30) {
     $snapdaysofmonth = "*"
 
 ### Create the new volume and set guarantees appropriately on na50
-    set-nasnapmirrorschedule -source $snapsourcena30 -Destination $destna50 -Minutes $snapminutes -Hours $snaphours -DaysOfWeek $snapdaysofweek -DaysOfMonth *  }
-$snapdestna50 = get-navol -$na50 | foreach-object { 
-    $_.Name 
-    $destna50 = "usoxf-na07:/vol/" }
-
-foreach ($snapsource in $snapschedna30) {
-
-    $snapsourcena30 = ($snapschedna30 | select-object Destination).Value
-    $snaprate = ($snapschedna30 | select-object MaxRate).Value
-    $snapminutes = ($snapschedna30 | select-object Minutes).Value
-    $snaphours = ($snapschedna30 | select-object Hours).Value
-    $snapdaysofweek = ($snapschedna30 | select-object DaysOfWeek).Value
-
-### Create the new volume and set guarantees appropriately on na50
-    set-nasnapmirrorschedule -source $snapsourcena30 -Destination $destna50 -Minutes $snapminutes -Hours $snaphours -DaysOfWeek $snapdaysofweek -WhatIf }
-
+    set-nasnapmirrorschedule -source $snapsourcena30 -Destination $destna50 -Minutes $snapminutes -Hours $snaphours -DaysOfWeek $snapdaysofweek -DaysOfMonth * }
 
