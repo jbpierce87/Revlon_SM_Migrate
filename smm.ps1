b965fb5c38e552de034366dcfc931fdd980910b2
Bacc <dragload@embarqmail.com>
2013-09-16 14:57:23 -0400

    added new test input file

--- smm.ps1
+++ smm.ps1
@@ -26,15 +26,17 @@
 
 .PARAMETER cleanup
 #>
-#param (
-#    [string]$workbook = $(throw "-workbook filename.xlsx is required"),
-#    [string]$logfile = $(throw "-log logfilename.txt is required"),
-#    [bool]$cleanup = $true
-# )
+param (
+    [string]$csvfile = $(throw "-csv filename.csv is required"),
+    [string]$logfile = $(throw "-log logname.txt is required"),
+    [bool]$cleanup = $true
+)
 import-module DataONTAP
 
-### Controller Login Variables
+function checkparams() {
+}
 
+### Controller Login Variables
 $ntap07 = "usoxf-na07"
 $ntap30 = "usedn-na30"
 $ntap50 = "usedn-na50"
@@ -46,62 +48,30 @@ $ntappw = "AcmeL4b#"
 $pw = convertto-securestring $ntappw -asplaintext -force
 $cred = new-object -typename system.management.automation.pscredential -argumentlist $ntapuser,$pw
 
-### Connect to the controllers 
-$na07 = Connect-NaController $ntap07 -Credential $cred -https
-$na30 = Connect-NaController $ntap30 -Credential $cred -https
-$na50 = Connect-NaController $ntap50 -Credential $cred -https
+### Connect to the controllers
+#$na07 = Connect-NaController $ntap07 -Credential $cred -https
+#$na30 = Connect-NaController $ntap30 -Credential $cred -https
+#$na50 = Connect-NaController $ntap50 -Credential $cred -https
 
-### Get volumes from the controllers as a test
-get-navol -controller $na07
-get-navol -controller $na30
-get-navol -controller $na50
+#$sourcefile = resolve-path $csvfile
+$csvobjects = Import-Csv -Path (resolve-path $csvfile).Path
 
-### Set aggregate variable where new volumes are going to be created
-$na50_aggr = "aggr3_3t"
-
-
-### Get volumes from na30 except for clones and vol0
-$na30_vols = Get-NaVol -Controller $na30 | ? {$_.CloneParent -eq $null} | ? {$_.Name -ne "vol0"} 
-
-### Get volume properties for each volume
-foreach ($vol in $na30_vols) {
-
-    $size = Get-NaVolSize -Controller $na30 -Name $vol.Name
-    $guarantee = ((Get-NaVolOption -controller $na30 -Name $vol.Name) | ? { $_.Name -eq "actual_guarantee" }).Value
-    $fracres_percent = ((Get-NaVolOption -Controller $na30 -Name $vol.Name) | ? { $_.Name -eq "fractional_reserve" }).Value
-    $snapres_percent = (Get-NaSnapshotReserve -Controller $na30 -TargetName $vol.Name).Percentage
-
-
-### Create the new volume and set guarantees appropriately on na50
-    New-NaVol -Controller $na50 -Name $vol.Name -Aggregate $na50_aggr -size $size.VolumeSize -SpaceReserve $guarantee }
-
-### Get Snapmirror schedules from na30 and assign variables
-$snapschedna30 = Get-NaSnapmirrorSchedule -Controller $na30 
-$snapschednadest30 = Get-NaSnapmirrorSchedule -Controller $na30 | select-object Destination | out-string
-$snapdestna50 = get-navol -Controller $na50 | ? {$_.CloneParent -eq $null} | ? {$_.Name -ne "vol0"} | foreach-object { 
-    $_.Name 
-    $destna50 = "usedn-na50:/vol/" + $_.Name } 
-
-foreach ($snapsource in $snapschedna30) {
-
-    $snapsourcena30 = $snapschedna30 | select-object Destination | out-string
-    $snaprate = ($snapschedna30 | select-object MaxRate).Value | out-string
-    $snapminutes = $snapschedna30 | select-object Minutes | out-string
-    $snaphours = $snapschedna30 | select-object Hours | out-string
-    $snapdaysofweek = $snapschedna30 | select-object DaysOfWeek | out-string
-
-### Having trouble with daysofmonth converting to string for input as variable ******
-    $snapdaysofmonth = "*"
-
-### Set Snapmirror schedule on new destination na07
-    set-nasnapmirrorschedule -source $snapsourcena30 -Destination $destna50 -Minutes $snapminutes -Hours $snaphours -DaysOfWeek $snapdaysofweek -DaysOfMonth * }
-
-### Initialize SnapMirror from old destination (na30) to new destination (na50)
-$snapmirrordestna50 = Get-NaSnapmirrorSchedule -Controller $na50 | select-object Destination | out-string
-
-foreach ($snapsource2 in $snapmirrordestna50) {
-
-    $snapsourcena30 = $snapschedna50 | select-object Destination | out-string
-    Invoke-NaSnapmirrorInitialize -source $snapsourcena30 -Destination $destna50 }
-
-
+#$sources = @()
+$sources += ($csvobjects | foreach-object {$_.SRC}) | select -uniq
+foreach ($src in $sources) {
+    "src: " + $src
+    $dynvars += New-Variable -Name "var$src" -Value $src
+}
+write-host "=========== dynvars ============"
+$dynvars
+#$na30 = Connect-NaController $ntap30 -Credential $cred -https
+#$na50 = Connect-NaController $ntap50 -Credential $cred -https
+$csvobjects | foreach-object {
+#    $_.SRC
+#    $_.SRCVOL
+#    $_.ODST
+#    $_.ODSTVOL
+#    $_.NDST
+#    $_.NDSTVOL
+#    $_.NDSTAGGR
+}
